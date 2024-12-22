bea72f24109833ce4ccdf93f69324fb16cc1b855
Zaid Alshamaa <zshamaa20@gmail.com>
2024-12-22 14:50:25 +0300

    Start Coding !

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Articles from "./pages/Articles/Articles";
+import Tips from "./pages/Tips/Tips";
+import Reminders from "./pages/Reminders/Reminders";
+
 function App() {
   return (
-    <div className="w-full h-full justify-center flex items-center">
-      <i className="fa-solid fa-camera"></i>
+    <div clasName="">
+      <Routes>
+        <Route path = "/" element = {<Articles />}/>
+        <Route path = "/reminders" element = {<Reminders />}/>
+        <Route path = "/tips" element = {<Tips />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Reminders/Reminders.js
@@ -0,0 +1,10 @@
+import Header from "../../components/Header/Header";
+
+export default function Reminders() {
+  return (
+    <>
+      <Header />
+      <p>reminders</p>
+    </>
+  );
+}
